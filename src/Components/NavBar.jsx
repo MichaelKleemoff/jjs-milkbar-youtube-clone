344f221faa47618fbda1f136349972e6e1d38cab
Lamb144 <alvinjohnson144@gmail.com>
2023-08-18 13:13:37 -0400

    Rough NavBar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+function NavBar() {
+    return (
+        <header className="header">
+            <div className="btn">
+                <a href="https://www.google.com">Home</a>
+                < a href="https://www.google.com">About</a>
+            </div>
+        </header>
+    )
+}
+export default NavBar;
