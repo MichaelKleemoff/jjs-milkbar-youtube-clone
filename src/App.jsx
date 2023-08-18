7a9c54a2883f4be83dc0b0b89b532677efb2cb06
jameel914 <jameelibrahim107@gmail.com>
2023-08-18 13:45:01 -0400

    mmm

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,11 @@ import SearchBar from './Components/SearchBar';
 import './App.css';
 
 function App() {
-<<<<<<< HEAD
-	return <>
-	<SearchBar />
-	</>;
-=======
 
 	return <>
 	<SearchBar />
 	</>;
 
->>>>>>> ac8824a6909f94252c73577f415e51784184f26d
 }
 
 export default App;
