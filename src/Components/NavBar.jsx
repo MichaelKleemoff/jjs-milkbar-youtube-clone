3ade376199a23842a9b4736bcac8dee654a5bd5f
Lamb144 <alvinjohnson144@gmail.com>
2023-08-21 10:28:33 -0400

    updated css for nav bar , home and about links need functionality ,  functions created

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,21 @@
 function NavBar() {
     return (
-        <header>
-            <div className="container">
-                <img src="src/assets/play-button-icon-png-18908.png" alt="redplaybutton" className="logo" />
-                <nav>
-                    <ul>
-                        {/* Home and about href links will be added later. */}
-                        <li><a href="#">home</a></li>
-                        <li><a href="#">about</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+        <>
+            <header>
+                <div className="container">
+                    <p className="milkbar">MilkBar</p>
+                    <img src="src/assets/play-button-icon-png-18908.png" alt="redplaybutton" className="logo" />
+                    <nav>
+                        <ul>
+                            {/* Home and about href links will be added later. */}
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">about</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </>
+
     )
 }
 export default NavBar; 
