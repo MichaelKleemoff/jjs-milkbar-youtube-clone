3148cece1502395370020b00d9e5737632d8ecee
Rubby <rubenslombard@pursuit.org>
2023-08-20 22:27:38 -0400

    Home commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+
+const Home = () => { 
+     
+
+    return (
+        <>
+            <div>Home</div>
+        </>
+    )
+ }
+export default Home:
