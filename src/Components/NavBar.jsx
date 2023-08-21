cc16c0f502ec05c94edae87f0a6ad613a58add23
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-08-21 05:48:52 -0400

    my last commit was lost, so I had to recode it - About Page

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
+import navBarLogo from "../images/yt_navBar_logo_red.png";
+import "../App.css";
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div id="header">
-      <div>LOGO</div>
-      <div id="homeLink">
-        <Link>Home</Link>{" "}
+    <>
+      <div className="container" id="header">
+        <div className="mr-auto p-2 bd-highlight">
+          <img src={navBarLogo} alt="YouTube Logo" height="60" id="logo-bar" />
+        </div>
+        <div className="d-flex align-items-center" id="homeLink">
+          <Link classNameq="mx-2" to="/">
+            Home
+          </Link>{" "}
+        </div>
+        <div className="d-flex align-items-center" id="aboutLink">
+          <Link className="mx-2" to="/about">
+            About
+          </Link>
+        </div>
       </div>
-      <div id="aboutLink">
-        <Link to="/about">About</Link>
-      </div>
-    </div>
+    </>
   );
 }
 
