b58691e584d0bb5d890bdec168a8f28f0c30a245
Michael Kleemoff <96216513+MichaelKleemoff@users.noreply.github.com>
2023-08-22 12:02:31 -0400

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
 
     return (
         <>
+             <SearchBar />
             <div className="homeForm">
                 <button> Addd a Comment </button>
             </div>
