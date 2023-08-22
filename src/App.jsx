bb730cde9f3312996a035f74a4bbea117f5feba8
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-08-22 19:36:29 -0400

    rebase merge conflct

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Components/Home.jsx';
-import About from './Components/About.jsx';
-import NavBar from './Components/NavBar.jsx';
-import Show from './Components/Show.jsx';
-import SearchBar from './Components/SearchBar';
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home.jsx";
+import About from "./Components/About.jsx";
+import NavBar from "./Components/NavBar.jsx";
+import Show from "./Components/Show.jsx";
+import SearchBar from "./Components/SearchBar";
+import "./App.css";
 
 function App() {
-	const apiUrl = import.meta.env.VITE_API_BASE_URL;
-	const apiKey = import.meta.env.VITE_API_KEY;
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+  const apiKey = import.meta.env.VITE_API_KEY;
 
-	return (
-		<div className='App'>
-			<Router>
-				<NavBar />
-				<Routes>
-					<Route path='/' element={<Home apiUrl={apiUrl} apiKey={apiKey} />} />
-					<Route path='/SearchBar' element={<SearchBar />} />
-					<Route path='/about' element={<About />} />
-					<Route
-						path='/videos/:id'
-						element={<Show apiUrl={apiUrl} apiKey={apiKey} />}
-					/>
-				</Routes>
-			</Router>
-		</div>
-	);
-
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home apiUrl={apiUrl} apiKey={apiKey} />} />
+          <Route path="/SearchBar" element={<SearchBar />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/videos/:id"
+            element={<Show apiUrl={apiUrl} apiKey={apiKey} />}
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
