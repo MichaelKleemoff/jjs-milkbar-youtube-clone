bd5bb7cf46c429d00a46e550a2e40c8dc2f1a228
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-08-23 12:47:56 -0400

    fixed apikey

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,31 +1,28 @@
-
-import { Link } from 'react-router-dom';
-import navBarLogo from '../images/yt_navBar_logo_red.png';
-import '../App.css';
-import './NavBar.css';
+import { Link } from "react-router-dom";
+import navBarLogo from "../images/yt_navBar_logo_red.png";
+import "../App.css";
+import "./NavBar.css";
 
 function NavBar() {
-
-	return (
-		<>
-			<div className='container' id='header'>
-				<div className='mr-auto p-2 bd-highlight'>
-					<img src={navBarLogo} alt='YouTube Logo' height='60' id='logo-bar' />
-				</div>
-				<div className='d-flex align-items-center' id='homeLink'>
-					<Link classNameq='mx-2' to='/'>
-						Home
-					</Link>{' '}
-				</div>
-				<div className='d-flex align-items-center' id='aboutLink'>
-					<Link className='mx-2' to='/about'>
-						About
-					</Link>
-				</div>
-			</div>
-		</>
-	);
-
+  return (
+    <>
+      <div className="container" id="header">
+        <div className="mr-auto p-2 bd-highlight">
+          <img src={navBarLogo} alt="YouTube Logo" height="60" id="logo-bar" />
+        </div>
+        <div className="d-flex align-items-center" id="homeLink">
+          <Link className="mx-2" to="/">
+            Home
+          </Link>{" "}
+        </div>
+        <div className="d-flex align-items-center" id="aboutLink">
+          <Link className="mx-2" to="/about">
+            About
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NavBar;
